614fc9962e93de83304002cd344f71b6796692a1
muhammednazeer <nazeer4u2c@gmail.com>
2020-08-16 14:53:36 +0100

    Add a new script

---
+++ exceptional.py
@@ -0,0 +1,7 @@
+"""A module for demonstrating exceptions."""
+
+
+def convert(s):
+    """Convert to an integer."""
+    x = int(s)
+    return x
--- words.py
+++ words.py
@@ -1,9 +1,24 @@
-from urllib.request import urlopen
+#!/usr/bin/env python3
 
-# This fetch the words and return them as a list
+
+"""Retrieve and print words from a URL.
+Usage:
+python3 words.py < URL >
+"""
+
+from urllib.request import urlopen
 
 
 def fetch_words():
+    """Fetch a list of words from a URL.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+
+    Returns:
+        A list of strings containing words from 
+        the document.
+    """
     with urlopen('http://sixty-north.com/c/t.txt') as story:
         story_words = []
         for line in story:
@@ -12,15 +27,23 @@ def fetch_words():
                 story_words.append(word)
     return story_words
 
-# This print a list of words
 
+def print_items(items):
+    """Print items one per line.
 
-def print_words(story_words):
-    for word in story_words:
-        print(word)
+    Args:
+        items: An iterable series of printable items.
+    """
+    for item in items:
+        print(item)
 
 
 def main():
+    """Print each word from a word document at a URL.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+    """
     words = fetch_words()
     print_words(words)
 
